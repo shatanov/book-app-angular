adef832560075219c680bb719c56763538ee88ca
shatanov <timur-shatanov.work@mail.ru>
2022-11-02 23:13:28 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'book-app-angular';
-}
+export class AppComponent {}
